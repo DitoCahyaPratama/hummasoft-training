2547a3bc1eb478bf86b71f1c8be5182993dd3d47
DitoCahyaPratama <ditocahyapratama717@gmail.com>
2022-06-28 08:53:37 +0700

    first commit

---
+++ javascript/namefile.js
@@ -0,0 +1,6 @@
+var kalimat = 'Hello World 1' //string
+var angka = 25 //integer
+var is_true = true //boolean
+var decimal = 1.2 //double
+var motor = ['Yamaha', 'Honda', 'Suzuki'] //array
+var biodata = { first_name: 'rezky', last_name: 'putra', age: 24 } //object
